d6880c698782cd1227ee6117f990f7f1f567eb94
tahera2 <161456813+tahera2@users.noreply.github.com>
2024-03-14 12:20:01 +0300

    Add files via upload

---
+++ simple_cv.py
@@ -0,0 +1,18 @@
+name_surname = input("Please enter your name :  ")
+University_name = input("your University name :  ")
+department = input("your department : ")
+internship = input("where have you done your first internship :  ")
+month = input("how many months :  ")
+job = input("as which job : ")
+online_course = input("online courses for your career : ")
+online_course_website = input("your course website : ")
+gained_skills = input("what skills have you gained on that course ? ")
+short_term_goals = input("what are your short term goals for company ? ")
+long_time_goals = input("what are your long term goals for company ? ")
+
+print(f"This is me {name_surname}. I graduated from the University of {University_name} with a bachelor's degree in \
+{department}. I completed my internship at {internship} for {month} months as an/a {job}. \
+I also finished an online course in {online_course} at {online_course_website} that helped me \
+improve my {gained_skills} skills. As I start working with your company, \
+my short-term goals are to {short_term_goals} also to improve my creative skills. Aside from that, I \
+aim to {long_time_goals}.")
