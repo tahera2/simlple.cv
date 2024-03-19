be6450fec202fd82f6cddda225451389eff9d6e3
tahera2 <161456813+tahera2@users.noreply.github.com>
2024-03-19 15:50:14 +0300

    Add files via upload
    a simple calculator doing the basic 4 operators

---
+++ calculator.py
@@ -0,0 +1,28 @@
+a= float(input ("enter number :  "))
+b= float(input ("enter number :  "))
+c= input ("choice a operation :  ")
+
+if c=="+" :
+    v=float(a+b)
+    print(v)
+elif c=="-":
+    v=float(a-b)
+    print(v)
+elif c=="*":
+    v=float(a*b)
+    print(v)
+elif c=="/":
+    v=float(a/b)
+    print(v)
+elif c=="":
+    v=float(a-b)
+    print(v)
+elif c=="-":
+    v=float(a-b)
+    print(v)
+elif c=="-":
+    v=float(a-b)
+    print(v)
+elif c=="-":
+    v=float(a-b)
+    print(v)
